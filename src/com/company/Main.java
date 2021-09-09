51c4d8c609b18f84a1230db87af52eb2f42a7907
AlluetV <alluetvalenzuela@gmail.com>
2021-09-08 22:30:30 -0500

    delete application

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import restaurant.Menu;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Menu menu = new Menu();
+        menu.populatedMenu();
+        menu.printMenu();
     }
 }
